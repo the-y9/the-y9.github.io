169b5f3fe29cc909e3ba924cc3f4bb64d7aea5a4
Yash Mishra <21f1006461@ds.study.iitm.ac.in>
2025-01-16 11:45:57 +0530

    16 jan

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// Initialize typed.js
+var options = {
+    strings: ["Data Science", "Student"],
+    typeSpeed: 50,
+    backSpeed: 25,
+    backDelay: 1000,
+    startDelay: 500,
+    loop: true
+};
+
+var typed = new Typed(".text-slider-items", options);
+
+// Smooth scrolling
+window.addEventListener('scroll', function() {
+    const navbar = document.getElementById('nav1');
+    if (window.scrollY > 50) { // Change 50 to any pixel value to trigger the scroll effect
+      navbar.classList.add('navbar-scrolled');
+    } else {
+      navbar.classList.remove('navbar-scrolled');
+    }
+  });
